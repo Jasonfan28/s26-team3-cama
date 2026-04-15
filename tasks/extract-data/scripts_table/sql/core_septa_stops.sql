c31e3686c0d119cab50f8d5a7d27317684e46e97
hssherr <163057897+hssherr@users.noreply.github.com>
2026-04-15 12:38:20 -0400

    fixed sql linting errors

--- tasks/extract-data/scripts_table/sql/core_septa_stops.sql
+++ tasks/extract-data/scripts_table/sql/core_septa_stops.sql
@@ -1,13 +1,14 @@
 CREATE OR REPLACE TABLE ${dataset_name_core}.core_phl_septa_stops AS
 SELECT
-  stopid,
-  `geometry`
+    stopid,
+    `geometry`
 FROM (
-  SELECT *,
-    ROW_NUMBER() OVER (
-      PARTITION BY stopname
-      ORDER BY stopname
-    ) AS rn
-  FROM ${dataset_name_source}.source_phl_septa_stops
+    SELECT
+        *,
+        ROW_NUMBER() OVER (
+            PARTITION BY stopname
+            ORDER BY stopname
+        ) AS rn
+    FROM ${dataset_name_source}.source_phl_septa_stops
 )
 WHERE rn = 1;
