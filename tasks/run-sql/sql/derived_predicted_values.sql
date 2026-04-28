5a49457166f190047ac215b62b88ad80a5cf9a7b
hssherr <163057897+hssherr@users.noreply.github.com>
2026-04-28 11:26:55 -0400

    lint sql for completed overall workflow

--- tasks/run-sql/sql/derived_predicted_values.sql
+++ tasks/run-sql/sql/derived_predicted_values.sql
@@ -1,14 +1,14 @@
 CREATE OR REPLACE TABLE ${dataset_name_derived}.derived_predicted_values AS
 SELECT
-  parcel_id,
-  CAST(ROUND(predicted_sale_price, -2) AS INT64) AS pred_value
+    parcel_id,
+    CAST(ROUND(predicted_sale_price, -2) AS INT64) AS pred_value
 FROM
-  ML.PREDICT(
-    MODEL ${dataset_name_derived}.model_lin_reg_tuned,
-    (
-      SELECT
-        * EXCEPT (sale_date)
-      FROM
-        ${dataset_name_derived}.derived_opa_properties
-    )
-  );
+    ML.predict (
+        MODEL ${dataset_name_derived}.model_lin_reg_tuned,
+        (
+            SELECT
+                * EXCEPT (sale_date)
+            FROM
+                ${dataset_name_derived}.derived_opa_properties
+        )
+    );
